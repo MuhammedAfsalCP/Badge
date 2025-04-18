1a3ae7113ec4d6bef54004f4a10aef878661aed6
MuhammedAfsalCP <muhammedafsalcp8@gmail.com>
2025-04-18 11:44:44 +0530

    class

---
+++ class.py
@@ -0,0 +1,13 @@
+# # class
+# # class is bluepring for creating objects
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name  # attribute
+#         self.age = age
+
+#     def greet(self):  # method
+#         print(f"Hello, my name is {self.name} and I am {self.age} years old.")
+
+# # Creating an object
+# p1 = Person("Afsal", 22)
+# p1.greet()
